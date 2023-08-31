946011fad4cc2139b86460a4ab40f5c4bdc13278
msyoki <msyokimutua@gmail.com>
2023-08-31 15:46:49 +0300

    add stock alert manager class

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -21,7 +21,23 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     subcategory = models.ForeignKey(SubCategory, on_delete=models.DO_NOTHING)
-   
+
+class StockAlert(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    threshold = models.PositiveIntegerField(default=5)
+    is_active = models.BooleanField(default=True)
+
+    def check_and_alert(self):
+        if self.is_active and self.product.quantity <= self.threshold:
+            # Here you can implement your alert mechanism, such as sending notifications
+            alert_message = f"ALERT: Product '{self.product.name}' is low in stock (current quantity: {self.product.quantity})"
+            print(alert_message)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.check_and_alert()
+
+ 
 
 class Sale(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -37,5 +53,13 @@ class Sale(models.Model):
         if self.pk is None:  # This checks if it's a new sale
             self.product.quantity -= self.quantity
             self.product.save()
+
+            # Get the StockAlert for the product
+            stock_alert = StockAlert.objects.get(product=self.product)
+
+            # Check and update the StockAlert
+            if self.product.quantity <= stock_alert.threshold:
+                stock_alert.is_active = True
+                stock_alert.save()
 
         super().save(*args, **kwargs)
